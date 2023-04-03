c4281393a0f78c8db65f79d0b84293a96c3303e5
Chekozhab <119935492+Chekozhab@users.noreply.github.com>
2023-04-03 13:14:57 +0300

    .

--- diplom/main/views.py
+++ diplom/main/views.py
@@ -5,11 +5,3 @@ from django.views.generic import  TemplateView
 
 class MainView(TemplateView):
     template_name = "main/index.html"
-def index(request):
-    return render(request, 'main/index.html', )
-
-
-def otz_slieder(request):
-    def any_view(request):
-        otzv = otz.objects.get(id=1)
-        return render(request,"index.html", {'otzv': otzv})
