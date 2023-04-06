c9d24d476f429f71b26616ae45123934500cb627
Chekozhab <119935492+Chekozhab@users.noreply.github.com>
2023-04-06 09:47:29 +0300

    .

--- diplom/main/models.py
+++ diplom/main/models.py
@@ -4,8 +4,8 @@ from django import forms
 
 class CorouselCommon(models.Model):
     title = models.CharField(verbose_name='Название', max_length=15, default="")
-    description = models.CharField(verbose_name='Название', max_length=15, default="")
-    image = models.ImageField(upload_to='uploads/% Y/% m/% d/', verbose_name='Изображение')
+    description = models.CharField(verbose_name='Название', max_length=40, default="")
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/', verbose_name='Изображение', max_length=500)
 
     def __str__(self):
         return self.title
--- diplom/main/views.py
+++ diplom/main/views.py
@@ -5,3 +5,11 @@ from django.views.generic import  TemplateView
 
 class MainView(TemplateView):
     template_name = "main/index.html"
+
+    def get_context_data(self,**kwargs):
+       context= super().get_context_data(**kwargs)
+       context['my_record']= CoroselReviews.objects.all()
+       return context
+
+
+
