fe44627794215e104123b39eb7af3f756379f973
Chekozhab <119935492+Chekozhab@users.noreply.github.com>
2023-04-19 21:10:50 +0300

    .4

--- diplom/main/views.py
+++ diplom/main/views.py
@@ -10,6 +10,8 @@ class MainView(TemplateView):
        context= super().get_context_data(**kwargs)
        context['my_record']= CoroselReviews.objects.all()
        context['my_record_assort'] = CoroselProducts.objects.all()
+       context['my_record_mobile'] = CoroselReviews.objects.first()
+       context['my_record_assort_mobile'] = CoroselProducts.objects.first()
        return context
 
 
